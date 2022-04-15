ab78eacd524e5884a3f7f7ed1e2723e09ce9ecb3
fridavbg <fperssontech@gmail.com>
2022-04-15 13:03:08 +0200

    Home page

---
+++ report/src/Controller/ReportController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        $data = [
+            'title' => 'HOME'
+        ];
+        return $this->render('home.html.twig', $data);
+    }
+}
